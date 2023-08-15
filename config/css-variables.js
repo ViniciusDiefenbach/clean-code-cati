04f51a3a4e69adaac477c4dd980bb23a015eab5a
Vinicius Diefenbach <viniciusdiefenbach2004@gmail.com>
2023-08-15 11:19:45 -0300

    create css variables

---
+++ config/css-variables.js
@@ -0,0 +1,4 @@
+export const backgroundColor1 = "#fff";
+export const backgroundColor2 = "#0D8623";
+export const fontColor1 = "#000";
+export const fontColor2 = "#fff";
---
+++ global.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import { backgroundColor1 } from "./config/css-variables";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: backgroundColor1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
