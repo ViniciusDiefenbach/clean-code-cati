b4d7cf4fa821c0b5ee4e410a78a1363cec729b5e
Vinicius Diefenbach <viniciusdiefenbach2004@gmail.com>
2023-08-18 19:22:56 -0300

    create app-images.js

---
+++ config/app-images.js
@@ -0,0 +1,5 @@
+import header from '../assets/header.png'
+
+export {
+  header
+}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import headerImage from '../../../assets/header.png'
 import { View, Text, Image } from 'react-native'
 import { styles } from './styles'
+import { header } from '../../../config/app-images'
 
 export default function Header () {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Projeto Cati</Text>
-      <Image source={headerImage} style={styles.image} />
+      <Image source={header} style={styles.image} />
     </View>
   )
 }
