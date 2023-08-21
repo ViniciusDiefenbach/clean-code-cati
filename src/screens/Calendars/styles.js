74e96b5105a5989ce0ed83f93a4c82974b92f5c7
Vinicius Diefenbach <viniciusdiefenbach2004@gmail.com>
2023-08-20 22:11:42 -0300

    calendars

--- src/screens/Calendars/styles.js
+++ src/screens/Calendars/styles.js
@@ -1,9 +1,22 @@
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
 import { backgroundColor1 } from '../../../config/app-colors'
+
+const { width } = Dimensions.get('window')
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: backgroundColor1
+  },
+  contentContainer: {
+    paddingVertical: 12,
+    paddingHorizontal: width * 0.06,
+    gap: 12
+  },
+  scheduleContainer: {
+    gap: 6
+  },
+  image: {
+    backgroundColor: 'black'
   }
 })
