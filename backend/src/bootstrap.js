ca8d515ec82415b53ce1cd6b416eab757a6d5cfc
Vinicius Diefenbach <viniciusdiefenbach2004@gmail.com>
2023-08-31 16:19:55 -0300

    Configure to use static files

--- backend/src/bootstrap.js
+++ backend/src/bootstrap.js
@@ -7,6 +7,7 @@ const { guideRoutes } = require("./routes/guide.routes");
 server.use(express.json());
 server.use(cors());
 server.use("/guide", guideRoutes);
+server.use(express.static("public"));
 
 server.listen(3000, () => {
   console.log("Server is running...");
